2a5c9039ba79773a8bd3d0587e7aa4556b296ac7
Riley Berton <riley.berton@mlb.com>
2021-02-19 14:54:22 -0500

    fix alert icon color

--- src/CirconusAlertPanelDetail.tsx
+++ src/CirconusAlertPanelDetail.tsx
@@ -194,9 +194,18 @@ export const CirconusAlertPanelDetail: React.FC<Props> = ({ options, data, width
       color: fontColor,
     };
   }
+
+  const style_ok = {
+    fill: 'green',
+    color: 'green',
+  };
+  const style_alert = {
+    fill: 'red',
+    color: 'red',
+  };
   const heartSVG = (
     <span>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="alert-state-ok">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style={style_ok}>
         <path d="M12,20.8623a2.75115,2.75115,0,0,1-1.94922-.80468L3.83691,13.84277A6.27238,6.27238,0,0,1,12,4.36328a6.27239,6.27239,0,0,1,8.16309,9.47949l-6.21338,6.21387A2.75,2.75,0,0,1,12,20.8623Z"></path>
       </svg>
     </span>
@@ -204,13 +213,7 @@ export const CirconusAlertPanelDetail: React.FC<Props> = ({ options, data, width
 
   const heartBreakSVG = (
     <span>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        width="24"
-        height="24"
-        className="alert-state-alerting"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style={style_alert}>
         <g id="Layer_2" data-name="Layer 2">
           <g id="Layer_1-2" data-name="Layer 1">
             <path d="M18.17,1.85h0A6.25,6.25,0,0,0,12.12.23L9.42,6.56l2.83.71a1,1,0,0,1,.67,1.41l-2,4a1,1,0,0,1-.9.56,1.13,1.13,0,0,1-.44-.1h0a1,1,0,0,1-.46-1.33l1.4-2.89-2.77-.7a1,1,0,0,1-.65-.53,1,1,0,0,1,0-.83L9.58,1a6.27,6.27,0,0,0-7.73,9.77L9.3,18.18a1,1,0,0,0,1.42,0h0l7.45-7.46A6.27,6.27,0,0,0,18.17,1.85Z"></path>
